fe3c3461d401d1e470f062b6c1028c42e2499759
Justin Dorfman <justin.dorfman@sourcegraph.com>
2022-10-18 09:26:43 -0700

    Update routes.tsx

--- routes.tsx
+++ routes.tsx
@@ -1,227 +1,76 @@
-import * as React from 'react'
-
-import { Redirect, RouteComponentProps } from 'react-router'
-
-import { ThemeProps } from '@sourcegraph/shared/src/theme'
 import { lazyComponent } from '@sourcegraph/shared/src/util/lazyComponent'
 
-import { BatchChangesProps } from './batches'
-import { CodeIntelligenceProps } from './codeintel'
-import { communitySearchContextsRoutes } from './communitySearchContexts/routes'
-import { BreadcrumbsProps, BreadcrumbSetters } from './components/Breadcrumbs'
-import type { LayoutProps } from './Layout'
-import { PageRoutes } from './routes.constants'
-import { SearchPageWrapper } from './search/SearchPageWrapper'
-import { getExperimentalFeatures, useExperimentalFeatures } from './stores'
-import { ThemePreferenceProps } from './theme'
+import { LayoutRouteProps } from '../routes'
 
-const SiteAdminArea = lazyComponent(() => import('./site-admin/SiteAdminArea'), 'SiteAdminArea')
-const ExtensionsArea = lazyComponent(() => import('./extensions/ExtensionsArea'), 'ExtensionsArea')
-const SearchConsolePage = lazyComponent(() => import('./search/SearchConsolePage'), 'SearchConsolePage')
-const NotebookPage = lazyComponent(() => import('./notebooks/notebookPage/NotebookPage'), 'NotebookPage')
-const SignInPage = lazyComponent(() => import('./auth/SignInPage'), 'SignInPage')
-const SignUpPage = lazyComponent(() => import('./auth/SignUpPage'), 'SignUpPage')
-const UnlockAccountPage = lazyComponent(() => import('./auth/UnlockAccount'), 'UnlockAccountPage')
-const SiteInitPage = lazyComponent(() => import('./site-admin/init/SiteInitPage'), 'SiteInitPage')
-const CreateNotebookPage = lazyComponent(
-    () => import('./notebooks/createPage/CreateNotebookPage'),
-    'CreateNotebookPage'
+const KubernetesCommunitySearchContextPage = lazyComponent(
+    () => import('./Kubernetes'),
+    'KubernetesCommunitySearchContextPage'
 )
-const NotebooksListPage = lazyComponent(() => import('./notebooks/listPage/NotebooksListPage'), 'NotebooksListPage')
-const InstallGitHubAppSuccessPage = lazyComponent(
-    () => import('./org/settings/codeHosts/InstallGitHubAppSuccessPage'),
-    'InstallGitHubAppSuccessPage'
+const StackstormCommunitySearchContextPage = lazyComponent(
+    () => import('./StackStorm'),
+    'StackStormCommunitySearchContextPage'
 )
+const TemporalCommunitySearchContextPage = lazyComponent(
+    () => import('./Temporal'),
+    'TemporalCommunitySearchContextPage'
+)
+const O3deCommunitySearchContextPage = lazyComponent(() => import('./o3de'), 'O3deCommunitySearchContextPage')
+const ChakraUICommunitySearchContextPage = lazyComponent(
+    () => import('./chakraui'),
+    'ChakraUICommunitySearchContextPage'
+)
+const StanfordCommunitySearchContextPage = lazyComponent(
+    () => import('./Stanford'),
+    'StanfordCommunitySearchContextPage'
+)
+const CncfCommunitySearchContextPage = lazyComponent(() => import('./cncf'), 'CncfCommunitySearchContextPage')
+const JuliaCommunitySearchContextPage = lazyComponent(() => import('./Julia'), 'JuliaCommunitySearchContextPage')
 
-export interface LayoutRouteComponentProps<RouteParameters extends { [K in keyof RouteParameters]?: string }>
-    extends RouteComponentProps<RouteParameters>,
-        Omit<LayoutProps, 'match'>,
-        ThemeProps,
-        ThemePreferenceProps,
-        BreadcrumbsProps,
-        BreadcrumbSetters,
-        CodeIntelligenceProps,
-        BatchChangesProps {
-    isSourcegraphDotCom: boolean
-    isMacPlatform: boolean
-}
-
-export interface LayoutRouteProps<Parameters_ extends { [K in keyof Parameters_]?: string }> {
-    path: string
-    exact?: boolean
-    render: (props: LayoutRouteComponentProps<Parameters_>) => React.ReactNode
-
-    /**
-     * A condition function that needs to return true if the route should be rendered
-     *
-     * @default () => true
-     */
-    condition?: (props: LayoutRouteComponentProps<Parameters_>) => boolean
-}
-
-// Force a hard reload so that we delegate to the serverside HTTP handler for a route.
-function passThroughToServer(): React.ReactNode {
-    window.location.reload()
-    return null
-}
-
-/**
- * Holds all top-level routes for the app because both the navbar and the main content area need to
- * switch over matched path.
- *
- * See https://reacttraining.com/react-router/web/example/sidebar
- */
-export const routes: readonly LayoutRouteProps<any>[] = ([
+// Hack! Hardcode these routes into cmd/frontend/internal/app/ui/router.go
+export const communitySearchContextsRoutes: readonly LayoutRouteProps<any>[] = [
     {
-        path: PageRoutes.Index,
-        render: props =>
-            window.context.sourcegraphDotComMode && !props.authenticatedUser ? (
-                <Redirect to="https://about.sourcegraph.com" />
-            ) : (
-                <Redirect to={PageRoutes.Search} />
-            ),
-        exact: true,
+        path: '/kubernetes',
+        render: props => <KubernetesCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.Search,
-        render: props => <SearchPageWrapper {...props} />,
-        exact: true,
+        path: '/stackstorm',
+        render: props => <StackstormCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.SearchConsole,
-        render: props => {
-            const { showMultilineSearchConsole } = getExperimentalFeatures()
-
-            return showMultilineSearchConsole ? <SearchConsolePage {...props} /> : <Redirect to={PageRoutes.Search} />
-        },
-        exact: true,
+        path: '/temporal',
+        render: props => <TemporalCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.SearchNotebook,
-        render: () => <Redirect to={PageRoutes.Notebooks} />,
-        exact: true,
+        path: '/o3de',
+        render: props => <O3deCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.NotebookCreate,
-        render: props =>
-            useExperimentalFeatures.getState().showSearchNotebook && props.authenticatedUser ? (
-                <CreateNotebookPage {...props} authenticatedUser={props.authenticatedUser} />
-            ) : (
-                <Redirect to={PageRoutes.Notebooks} />
-            ),
-        exact: true,
+        path: '/chakraui',
+        render: props => <ChakraUICommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.Notebook,
-        render: props => {
-            const { showSearchNotebook, showSearchContext } = useExperimentalFeatures.getState()
-
-            return showSearchNotebook ? (
-                <NotebookPage {...props} showSearchContext={showSearchContext ?? false} />
-            ) : (
-                <Redirect to={PageRoutes.Search} />
-            )
-        },
-        exact: true,
+        path: '/stanford',
+        render: props => <StanfordCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.Notebooks,
-        render: props =>
-            useExperimentalFeatures.getState().showSearchNotebook ? (
-                <NotebooksListPage {...props} />
-            ) : (
-                <Redirect to={PageRoutes.Search} />
-            ),
-        exact: true,
+        path: '/cncf',
+        render: props => <CncfCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.SignIn,
-        render: props => <SignInPage {...props} context={window.context} />,
-        exact: true,
+        path: '/julia',
+        render: props => <JuliaCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
     {
-        path: PageRoutes.SignUp,
-        render: props => <SignUpPage {...props} context={window.context} />,
-        exact: true,
+        path: '/backstage',
+        render: props => <BackstageCommunitySearchContextPage {...props} />,
+        condition: ({ isSourcegraphDotCom }) => isSourcegraphDotCom,
     },
-    {
-        path: PageRoutes.UnlockAccount,
-        render: props => <UnlockAccountPage {...props} context={window.context} />,
-        exact: true,
-    },
-    {
-        path: PageRoutes.Welcome,
-        // This route is deprecated after we removed the post-sign-up page experimental feature, but we keep it for now to not break links.
-        render: props => <Redirect to={PageRoutes.Search} />,
-        exact: true,
-    },
-    {
-        path: PageRoutes.InstallGitHubAppSuccess,
-        render: () => <InstallGitHubAppSuccessPage />,
-    },
-    {
-        path: PageRoutes.Settings,
-        render: lazyComponent(() => import('./user/settings/RedirectToUserSettings'), 'RedirectToUserSettings'),
-    },
-    {
-        path: PageRoutes.User,
-        render: lazyComponent(() => import('./user/settings/RedirectToUserPage'), 'RedirectToUserPage'),
-    },
-    {
-        path: PageRoutes.Organizations,
-        render: lazyComponent(() => import('./org/OrgsArea'), 'OrgsArea'),
-    },
-    {
-        path: PageRoutes.SiteAdminInit,
-        exact: true,
-        render: props => <SiteInitPage {...props} context={window.context} />,
-    },
-    {
-        path: PageRoutes.SiteAdmin,
-        render: props => (
-            <SiteAdminArea
-                {...props}
-                routes={props.siteAdminAreaRoutes}
-                sideBarGroups={props.siteAdminSideBarGroups}
-                overviewComponents={props.siteAdminOverviewComponents}
-            />
-        ),
-    },
-    {
-        path: PageRoutes.PasswordReset,
-        render: lazyComponent(() => import('./auth/ResetPasswordPage'), 'ResetPasswordPage'),
-        exact: true,
-    },
-    {
-        path: PageRoutes.ApiConsole,
-        render: lazyComponent(() => import('./api/ApiConsole'), 'ApiConsole'),
-        exact: true,
-    },
-    {
-        path: PageRoutes.UserArea,
-        render: lazyComponent(() => import('./user/area/UserArea'), 'UserArea'),
-    },
-    {
-        path: PageRoutes.Survey,
-        render: lazyComponent(() => import('./marketing/page/SurveyPage'), 'SurveyPage'),
-    },
-    window.context.enableLegacyExtensions
-        ? {
-              path: PageRoutes.Extensions,
-              render: props => <ExtensionsArea {...props} routes={props.extensionsAreaRoutes} />,
-          }
-        : undefined,
-    {
-        path: PageRoutes.Help,
-        render: passThroughToServer,
-    },
-    {
-        path: PageRoutes.Debug,
-        render: passThroughToServer,
-    },
-    ...communitySearchContextsRoutes,
-    {
-        path: PageRoutes.RepoContainer,
-        render: lazyComponent(() => import('./repo/RepoContainer'), 'RepoContainer'),
-    },
-] as readonly (LayoutRouteProps<any> | undefined)[]).filter(Boolean) as readonly LayoutRouteProps<any>[]
+]
